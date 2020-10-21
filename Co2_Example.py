7edb5f8061f54b2ecfe2143eb472e31aaa6dc6e8
GloryKim <yklovejesus@gmail.com>
2020-10-21 11:42:21 +0900

    Co2_Example

--- Co2_Example.py
+++ Co2_Example.py
@@ -7,23 +7,23 @@ GPIO.setmode(GPIO.BOARD)
 # GPIO.BOARD 보드 상의 핀 번호 사용
 # GPIO.BCM .  핀번호가 아니라 Broadcom SOC channel을 사용 GPIOXX의 XX 번호를 사용
 
-# 10번 핀을 사용함
-pin_to_circuit = 10
+# 35번 핀을 사용함
+pin_to_circuit = 35
 
 def rc_time (pin_to_circuit):
     count = 0
 
     #Output on the pin for
-    GPIO.setup(pin_to_circuit, GPIO.OUT)   # 10번 핀을 입력으로 설정
-    GPIO.output(pin_to_circuit, GPIO.LOW)  # 10번 핀의 디지털 출력 설정
+    GPIO.setup(pin_to_circuit, GPIO.OUT)   # 35번 핀을 입력으로 설정
+    GPIO.output(pin_to_circuit, GPIO.LOW)  # 35번 핀의 디지털 출력 설정
  
                                                       
     time.sleep(0.1)  # 0.1 sec sleep
 
-    # 10번 핀을 input으로 변경
+    # 35번 핀을 input으로 변경
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
-    # 10번 핀으로부터 읽은 값이 HIGH가 될 때까지 count 수행
+    # 35번 핀으로부터 읽은 값이 HIGH가 될 때까지 count 수행
     # 그래서 실행해보면 센서 주변이 어두울 수록 카운트 값이 크다. 
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
         count += 1
