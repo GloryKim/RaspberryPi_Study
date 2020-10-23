a701ec961344310e98cd2f6abb088825a5d87459
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-10-23 12:09:55 +0900

    1.0.3

--- Shock/Touch.py
+++ Shock/Touch.py
@@ -1,15 +1,12 @@
-#아무것도 안만지면 출력이 전혀 없음
-#만지길 시작하면 0이상의 값을 0.1초 간격으로 출력함
 
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-# 35번 핀을 사용함
 pin_to_circuit = 35
 
 def rc_time (pin_to_circuit):
-    count = 1 #터치를 할때 1을 리턴
+    count = 1 #충격을 줄때 1을 리턴
 
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)                             
@@ -17,8 +14,7 @@ def rc_time (pin_to_circuit):
     GPIO.setup(pin_to_circuit, GPIO.IN)
 
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
-        count = 0 # 터치를 하는 순간 전압이 변함을 감지하여 0을 리턴
-        #즉 시작 할때는 0 만지고 있으면 계속 1를 리턴함
+        count = 1 # 충격 센서는 워낙 단순하게 구성되어있어서 단순히 디지털한 신호밖에 리턴을 안함
 
     return count
 
