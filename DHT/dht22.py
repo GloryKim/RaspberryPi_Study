ea601f23255d9ac71c2ed01731159ca1d3d7fb08
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-10-29 20:55:25 +0900

    1.0.5

--- DHT/dht22.py
+++ DHT/dht22.py
@@ -4,7 +4,8 @@ import time
 
 def print_dht22():
     h, t =dht.read_retry(dht.DHT22, 4)
-    print 'Temp={0:0.lf}*C Humidity={1:0.lf}%' . format(t, h)
+    f = (t*9/5)+32
+    print 'Fahrenheit={0:0.lf}*F Celsius={1:0.lf}*C Humidity={2:0.lf}%' . format(f, t, h)
     threading.Timer(2.5, print_dht22).start
 
 print_dht22()
